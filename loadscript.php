f36719be1c8ca12f366cee1122ff56bc45088f2d
kokushkin <fipsiksi@yandex.ru>
2015-03-10 02:09:38 +0400

    i must write loading to db

--- loadscript.php
+++ loadscript.php
@@ -1,23 +1,50 @@
 <?php
 
-$row=1;
-
-$connection = mysql_connect("localhost", "root", "") or die(mysql_error());
-$db = mysql_select_db("logbase", $connection) or die(mysql_error());
-
-
-
-if(($handle=fopen("LogFile.txt","r"))!=FALSE) 
+session_start();
+if (!isset($_SESSION['percent']))
 {
-	fgets($handle);
-	while(($data=fgetcsv($handle,1000,"	"))!==FALSE)
+	  $_SESSION['percent'] = 0;
+	  $connection = mysql_connect("localhost", "root", "") or die(mysql_error());
+	  $db = mysql_select_db("logbase", $connection) or die(mysql_error());
+	  $allrow = 0;
+	  if(($handle=fopen("LogFile.txt","r"))!=FALSE) 
+	  {
+		$allrow = 0;
+		$row=0;
+		while(fgets($handle)!==FALSE)
 		{
-		$num=count($data);
-		//вставляем в базу
-		$query = mysql_query("INSERT INTO systemlog(name,email) VALUES($sname,$semail)", $connection) or die(mysql_error());
-		$row++;
+			$allrow++;
 		}
-	fclose($handle);
+		fseek($handle, 0);
+		fgets($handle);
+		while(($data=fgetcsv($handle,1000,"\t"))!==FALSE)
+			{
+			//$num=count($data);
+			//вставляем в базу
+			//$mas = split("\t",$data);
+			$row++;
+			$_SESSION['percent'] = ($row/$allrow)*100;
+			if(count($data) === 5)
+				{
+				$query = mysql_query("INSERT INTO systemlog(Level,DateTime,Source,Code,Description) VALUES(mas[0],mas[1],mas[2],mas[3],mas[4])", $connection) or die(mysql_error());	
+				}
+			}
+		$_SESSION['percent']=100;
+		fclose($handle);
+	  }
+	  
+	  
 }
+else
+{
+	  echo($_SESSION['percent']);
+}
+
+
+
+
+
+
+
 
 ?>
