0c3389dfe2fc234ed69ac73e7390bcbbd9cb62bf
kokushkin <fipsiksi@yandex.ru>
2015-03-11 03:08:57 +0400

    duplicate key error

---
+++ loadfrombase.php
@@ -0,0 +1,20 @@
+<?php
+
+	beginrecord = $_POST['stringbegin'];
+	endrecord = $_POST['stringend'];
+	sortparam = $_POST['sort'];
+	
+	$connection = mysql_connect("localhost", "root", "") or die(mysql_error());	
+	$db = mysql_select_db("logbase", $connection) or die(mysql_error());
+	 
+	$query = mysql_query("SELECT Level,DateTime,Source,Code,Description FROM systemlog" )
+			$connection) or die(mysql_error());	
+	echo("{ [");
+	while ($row = mysql_fetch_array($result, MYSQL_NUM)) 
+	{
+		echo('{'.$row[0].','.$row[1].','$row[2]','$row[3]','$row[4]'}')
+	}
+	echo("] }");
+
+
+?>
--- loadscript.php
+++ loadscript.php
@@ -1,44 +1,60 @@
 <?php
 
 session_start();
-if (!isset($_SESSION['percent']))
-{
+//if (!isset($_SESSION['percent']))
+//{
+	  var_dump($_SESSION['percent']);
 	  $_SESSION['percent'] = 0;
 	  $connection = mysql_connect("localhost", "root", "") or die(mysql_error());
+	  var_dump($connection);
 	  $db = mysql_select_db("logbase", $connection) or die(mysql_error());
+	  var_dump($db);
 	  $allrow = 0;
 	  if(($handle=fopen("LogFile.txt","r"))!=FALSE) 
 	  {
+	  var_dump($handle);
 		$allrow = 0;
 		$row=0;
 		while(fgets($handle)!==FALSE)
 		{
 			$allrow++;
 		}
+		var_dump($allrow);
 		fseek($handle, 0);
 		fgets($handle);
 		while(($data=fgetcsv($handle,1000,"\t"))!==FALSE)
 			{
+			var_dump($data);
 			//$num=count($data);
 			//вставляем в базу
 			//$mas = split("\t",$data);
 			$row++;
+			var_dump($row);
 			$_SESSION['percent'] = ($row/$allrow)*100;
-			if(count($data) === 5)
+			var_dump($_SESSION['percent']);
+			var_dump(count($data));
+			if(count($data) === 6)
 				{
-				$query = mysql_query("INSERT INTO systemlog(Level,DateTime,Source,Code,Description) VALUES(mas[0],mas[1],mas[2],mas[3],mas[4])", $connection) or die(mysql_error());	
+				$query = "INSERT INTO systemlog(Level,DateTime,Source,Code,Description) VALUES('$data[0]','$data[1]','$data[2]','$data[3]','$data[5]')";
+				var_dump($query);
+				$result = mysql_query($query, $connection) or die(mysql_error());	
+				
+				var_dump($result);
 				}
 			}
 		$_SESSION['percent']=100;
+		mysql_close($connection);
 		fclose($handle);
+		unset($_SESSION['percent']);
+		session_destroy();
 	  }
 	  
 	  
-}
-else
-{
-	  echo($_SESSION['percent']);
-}
+// }
+// else
+// {
+	  // echo($_SESSION['percent']);
+// }
 
 
 
